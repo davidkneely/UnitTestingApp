e0a71c50f6e804d9243748b7a08037834a2ea635
David N <deeniri@gmail.com>
2015-02-15 19:49:06 -1000

    Updated project with code coverage using JUnit and JaCoCo.

---
+++ src/com/example/junit/MyClass.java
@@ -0,0 +1,22 @@
+package com.example.junit;
+
+/**
+ * Tests for code coverage using JUnit and JaCoCo.
+ */
+public class MyClass {
+  /**
+   * Prints Hello.
+   * @return The String.
+   */
+  public String sayHello() {
+    return "Hello";
+  }
+
+  /**
+   * Prints Bye.
+   * @return The String.
+   */
+  public String sayBye() {
+    return "Bye";
+  }
+}
---
+++ src/package-info.java
@@ -0,0 +1,3 @@
+/**
+ * Created by davidneely on 2/15/15.
+ */
